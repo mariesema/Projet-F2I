3e8e460b54cc152b66505092cf9b5199402c68f3
jpitard <johnny.pitard@gmail.com>
2016-04-14 00:38:10 +0200

    creation des classes disponibilité ficheConseil prefecture rendezVous

---
+++ dev/sw/WebSite2/App_Code/entites/Disponibilite.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace stopgaspi.sw.WebSite2.App_Code.entites
+{
+    class Disponibilite
+    {
+        private long id;
+
+        public long Id
+        {
+            get{ return id;}
+            set{id = value; }
+        }
+        private DateTime date;
+
+        public DateTime Date
+        {
+            get { return date;}
+            set { date = value;}
+        }
+        private DateTime heureDebut;
+
+        public DateTime HeureDebut
+        {
+            get { return heureDebut; }
+            set { heureDebut = value; }
+        }
+        private DateTime heureFin;
+
+        public DateTime HeureFin
+        {
+            get { return heureFin; }
+            set { heureFin = value; }
+        }
+
+        public Disponibilite()
+        {
+
+        }
+
+
+
+       
+    }
+}
---
+++ dev/sw/WebSite2/App_Code/entites/FicheConseil.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace stopgaspi.sw.WebSite2.App_Code.entites
+{
+    class FicheConseil
+    {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private string contenu;
+
+        public string Contenu
+        {
+            get { return contenu; }
+            set { contenu = value; }
+        }
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        private ModTypeDomaine domaine;
+
+        public ModTypeDomaine Domaine
+        {
+            get { return domaine; }
+            set { domaine = value; }
+        }
+        
+        public FicheConseil()
+        {
+
+        }
+    }
+}
