b1b9759643368ea2a4a694461d2949fca71ae819
mariesema <marie.sema@gmail.com>
2016-05-29 15:51:08 +0200

    commit_sw_user

--- dev/sw/index.php
+++ dev/sw/index.php
@@ -1,7 +1,32 @@
 <!DOCTYPE html>
 <html>
-    <p>lala</p>
+    <head>
+        <title>Accueil</title>
+        <meta charset="UTF-8"/>
+    </head>
     
+    
+    <body>
+    <div id="menu">
+	
+		<h2>Menu</h2>
+		
+		<ul>
+			<li><a href="index.php">Accueil</a></li>
+		</ul>
+		
+		<h3>Espace membre</h3>
+		<ul>
+                    <li><a href="views/inscription.php" >Inscription</a></li>
+		</ul>
+		
+	</div>
+    
+    </body>
+    
+    
+    
+        
     
     
     
--- dev/sw/views/inscription.php
+++ dev/sw/views/inscription.php
@@ -1,5 +1,6 @@
 <?php
-
+    namespace Models;
+    
 
 ?>
 
@@ -7,24 +8,33 @@
 <html>
 <head>
 	<title>Inscription</title>
-	<meta charset="utf-8"/>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css" />
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+	<meta charset="UTF-8"/>
+        <link rel="stylesheet" type="text/css" href="../css/bootstrap-theme.css" />
+        
+        
 </head>
 <body>
-<form method="POST" action="inscription">
-	Raison Sociale:  					<input type="text" name="raison_sociale"><br/>
-	Identifiant:  						<input type="text" name="identifiant"><br/>
-	Mot de passe:  	 					<input type="password" name="mot_de_passe"><br/>
-	Confirmer mot de passe :  			<input type="password" name="confirmer_mot_de_passe"><br/>
-	Adresse e-mail:  					<input type="mail" name="email"><br/>
-	Confirmer adresse e-mail:  			<input type="mail" name="confirmer_email"><br/>
-	Adresse postale:  					<input type="text" name="adresse"><br/>
-	Complément d'adresse:  				<input type="text" name="adresse2"><br/>
-	Ville: 								<input type="text" name="ville"><br/>
-	<input type="submit" name="btn_inscription">
-
-</form>
-
+    
+    
+    
+        <form id="form_inscrpiton" method="POST" action="inscription">
+            <fieldset>
+                <legend>Informations de connexion</legend>
+                <label> Identifiant:<input type="text" name="identifiant"></label><br/>
+                  <label>  Mot de passe:<input type="password" name="mot_de_passe"></label><br/>
+                    <label>Confirmer mot de passe : <input type="password" name="confirmer_mot_de_passe"></label><br/>
+            </fieldset>
+            <fieldset>
+                <legend>Informations  générales</legend>
+               <label> Raison Sociale: <input type="text" name="raison_sociale"/></label><br/>
+               <label>Adresse e-mail:  <input type="mail" name="email"/></label><br/>
+               <label> Confirmer adresse e-mail: <input type="mail" name="confirmer_email"/></label><br/>
+               <label> Adresse postale: <input type="text" name="adresse"/></label><br/>
+               <label> Complément d'adresse: <input type="text" name="adresse2"/></label><br/>
+               <label> Ville: <input type="text" name="ville"/></label><br/>
+                <input  class="boutons_formulaire" type="submit" name="btn_inscription" value="S'inscrire">
+            </fieldset>
+        </form>
+    
 </body>
 </html>
